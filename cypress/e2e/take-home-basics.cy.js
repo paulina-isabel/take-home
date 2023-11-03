0042801b1a2ada4f9e9809f282abf68d63e366d3
Paulina Rubio-Parra <paulinaisabel@me.com>
2023-11-03 16:01:32 -0400

    Stub fetch calls.

--- cypress/e2e/take-home-basics.cy.js
+++ cypress/e2e/take-home-basics.cy.js
@@ -1,5 +1,59 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('landing page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'https://newsapi.org/v2/top-headlines?country=us&apiKey=f101fd01d12c42e2877136a41b1b404c', {
+      statusCode: 200,
+      fixture: 'allArticles.json'
+    })
+    cy.visit('http://localhost:3000/')
   })
+
+  it('should show all movies when on the homepage', () => {
+    cy.location('pathname').should('eq', '/');
+    // cy.get('.header').should('exist');
+    // cy.get('.card').first().within(() => {
+    //   cy.get('img[name=movie-poster]').should('exist')
+    //   cy.contains('h2', 'Wifelike (2022');
+    //   cy.contains('h3', '6/10');
+    //   cy.get('.star').should('exist');
+    // })
+    // cy.get('.card').last().within(() => {
+    //   cy.get('img[name=movie-poster]').should('exist');
+    //   cy.contains('h2', 'X (2022');
+    //   cy.contains('h3', '1/10');
+    //   cy.get('.star').should('exist');
+    // })
+  })
+
+  // it('should display a helpful message to the user when an error occurs', () => {
+  //   cy.intercept(
+  //     "GET",
+  //     "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
+  //     {
+  //       statusCode: 500
+  //     }
+  //   ).visit("http://localhost:3000");
+  //   cy.get(".error-message").should('exist');
+  // })
+
+  // it('should display a helpful message to the user when an error occurs', () => {
+  //   cy.intercept(
+  //     "GET",
+  //     "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
+  //     {
+  //       statusCode: 400
+  //     }
+  //   ).visit("http://localhost:3000");
+  //   cy.get(".error-message").should('exist');
+  // })
+
+  // it('should display a helpful message to the user when an error occurs', () => {
+  //   cy.intercept(
+  //     "GET",
+  //     "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
+  //     {
+  //       statusCode: 300
+  //     }
+  //   ).visit("http://localhost:3000");
+  //   cy.get(".error-message").should('exist');
+  // })
 })
